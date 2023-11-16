dd4cc5aa231de6f14d5dee822dbfe378161a2192
zy-link <y_1st.zenith@icloud.com>
2023-11-15 23:37:06 -0800

    add taxonomy

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -42,9 +42,42 @@ function fithub_register_custom_post_types()
 }
 add_action('init', 'fithub_register_custom_post_types');
 
+function fithub_register_taxonomies()
+{
+    // Add Team Category taxonomy
+    $labels = array(
+        'name'              => _x('Team Categories', 'taxonomy general name'),
+        'singular_name'     => _x('Team Category', 'taxonomy singular name'),
+        'search_items'      => __('Search Team Categories'),
+        'all_items'         => __('All Team Category'),
+        'parent_item'       => __('Parent Team Category'),
+        'parent_item_colon' => __('Parent Team Category:'),
+        'edit_item'         => __('Edit Team Category'),
+        'view_item'         => __('Vview Team Category'),
+        'update_item'       => __('Update Team Category'),
+        'add_new_item'      => __('Add New Team Category'),
+        'new_item_name'     => __('New Team Category Name'),
+        'menu_name'         => __('Team Category'),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array('slug' => 'team-categories'),
+    );
+    register_taxonomy('fithub-team-category', array('fithub-team'), $args);
+}
+add_action('init', 'fithub_register_taxonomies');
+
 function fithub_rewrite_flush()
 {
     fithub_register_custom_post_types();
+    fithub_register_taxonomies();
     fithub_rewrite_flush();
 }
 add_action('after_switch_theme', 'fithub_rewrite_flush');
