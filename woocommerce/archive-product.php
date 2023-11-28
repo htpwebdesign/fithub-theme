4d9705eea458dbde86aab7aaa9055621094e7f5f
jestebanito <137816026+jestebanito@users.noreply.github.com>
2023-11-28 00:43:02 -0800

    added responsiveness to home, archive-product, team page, and product category page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -103,7 +103,7 @@ if ( woocommerce_product_loop() ) {
 								$product_query->the_post();
 						?>
 							<a href="<?php the_permalink(); ?>">
-								<?php the_post_thumbnail('medium_large'); ?>
+								<?php the_post_thumbnail('large'); ?>
 								<h3><?php the_title(); ?></h3>
 							</a>
 						<?php
